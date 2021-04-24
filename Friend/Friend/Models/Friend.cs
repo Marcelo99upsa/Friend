2181f5e990c22a3d415295606d3de62642816144
Marcelo Ortiz Peredo <a2018119133@estudiantes.upsa.edu.bo>
2021-04-24 09:45:54 -0400

    Se creo el Modelo Friend

---
+++ Friend/Friend/Models/Friend.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Friend.Models
+{
+    public class Friend
+    {
+        [Key]
+        public int FriendId { get; set; }
+        [Required]
+        [StringLength(30, ErrorMessage = "Ingresar Nombre entre a 3 a 30 Caracteres", MinimumLength = 3)]
+        public string Name { get; set; }
+        public string NickName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime BirthDay { get; set; }
+    }
+}
